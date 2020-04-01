cdf6f3576333f2ab5516638514ef080afbd45405
Gujinyu <gujinyu@sjtu.edu.cn>
2020-04-02 01:01:58 +0800

    retrieve each instructions on IR

--- Makefile
+++ Makefile
@@ -6,6 +6,7 @@
 # Generate IR (bitcode)
 default:
 	clang -O3 -emit-llvm app.c -c -o app.bc
+	llc app.bc -o app.s
 
 # Take a look at the LLVM assembly code
 # llvm-dis < app.bc
@@ -14,4 +15,4 @@ default:
 # llc app.bc -o app.s
 
 clean:
-	rm -f app.bc app.s
+	rm -f *.bc app.s
--- tmac_hello_pass/hello_pass.cpp
+++ tmac_hello_pass/hello_pass.cpp
@@ -10,27 +10,48 @@ int tmac_func_cnt = 0;
 namespace {
 
 	struct TmacHello : public ModulePass {
-	//struct TmacHello : public FunctionPass {
 		static char ID; /* Pass ID */
-		TmacHello() : ModulePass(ID) {}
-		//TmacHello() : FunctionPass(ID) {}
+		unsigned long inst_cnt;
 
-		//bool runOnFunction(Function &F) override {
-		bool runOnModule(Module &F) override {
-			for (Function &FF : F.getFunctionList()) {
-			++tmac_func_cnt;
-			errs() << "Hello: ";
-			errs().write_escaped(FF.getName()) << "\n";
+		TmacHello() : ModulePass(ID), inst_cnt(0) {}
+
+#if 0
+		/* Sample */
+		bool runOnModule(Module &m) override {
+			/* Traverse function list in the module */
+			for (Function &f : m.getFunctionList()) {
+				++tmac_func_cnt;
+				errs() << "hello: ";
+				errs().write_escaped(f.getName()) << "\n";
 			}
 			return false;
 		}
+#endif
+		bool runOnModule(Module &m) override;
 	};
+
+	/* tmac: Run on each module (file?) */
+	bool TmacHello::runOnModule(Module &m) {
+		/* Traverse function list in the module */
+		for (Function &f : m.getFunctionList()) {
+			for (BasicBlock &b : f.getBasicBlockList()) {
+				for (Instruction &inst : b.getInstList()) {
+					errs() << "Inst-" << inst_cnt++
+						<< " ";
+					errs().write_escaped(inst.getOpcodeName())
+						<< "\n";
+				}
+			}
+		}
+		return false;
+	}
 }
+
 
 /* Initial ID value is not used. The address of ID is used instead. */
 char TmacHello::ID = 0;
 
 /* @command-line arg: TmacHello; @name: Tmac Hello World Pass */
-static RegisterPass<TmacHello> X("TmacHello", "Tmac Hello World Pass");
+static RegisterPass<TmacHello> X("Jmpsafe", "Tmac: Jmpsafe Pass");
 			     //false, false);
 
